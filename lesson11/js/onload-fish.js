95386f8791fcc0fa2fe5d56948ebe897a819b88d
hol16046 <hol16046@byui.edu>
2019-11-29 02:27:12 -0700

    Lesson 11

---
+++ lesson11/js/onload-fish.js
@@ -0,0 +1,13 @@
+function AddOnloadEvent(f) {
+    if(typeof window.onload != 'function') {window.onload = f; }
+    else {
+        var cache = window.onload;
+        window.onload = function() {
+            if(cache) { cache(); }
+            f();
+        };
+    }
+}
+
+
+AddOnloadEvent (compute);
---
+++ lesson11/js/soda.js
@@ -0,0 +1,62 @@
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=05b93bf3cf5cea8551a4a182b8781249';
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        document.getElementById('temp').textContent = jsObject.main.temp.toFixed(0);
+        document.getElementById('condition').textContent = jsObject.weather[0].description;
+        document.getElementById('wind').textContent = jsObject.wind.speed.toFixed(0);
+        document.getElementById('humid').textContent = jsObject.main.humidity;
+    })
+
+const apiURLforecast = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=05b93bf3cf5cea8551a4a182b8781249';
+
+fetch(apiURLforecast)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        var num = 1;
+
+        jsObject.list.forEach(getData);
+
+        function getData(item){
+            if (item.dt_txt.includes("18:00:00")) {
+                document.getElementById('day' + num + 'pic').setAttribute('src', 'https://openweathermap.org/img/wn/' + item.weather[0].icon + '@2x.png');
+                document.getElementById('day' + num + 'pic').setAttribute('alt', item.weather[0].description + ' icon');
+                document.getElementById('day' + num + 'temp').textContent = item.main.temp.toFixed(0);
+                num++;
+            }
+        }
+
+    })
+    
+    var days = new Array(14);
+    days[0] = "Sun";
+    days[1] = "Mon";
+    days[2] = "Tue";
+    days[3] = "Wed";
+    days[4] = "Thu";
+    days[5] = "Fri";
+    days[6] = "Sat";
+    days[7] = "Sun";
+    days[8] = "Mon";
+    days[9] = "Tue";
+    days[10] = "Wed";
+    days[11] = "Thu";
+    days[12] = "Fri";
+    days[13] = "Sat";
+
+    var first = days[d.getDay() + 1];
+    var second = days[d.getDay() + 2];
+    var third = days[d.getDay() + 3];
+    var fourth= days[d.getDay() + 4];
+    var fifth = days[d.getDay() + 5];
+
+    document.getElementById('day1').textContent = first;
+    document.getElementById('day2').textContent = second;
+    document.getElementById('day3').textContent = third;
+    document.getElementById('day4').textContent = fourth;
+    document.getElementById('day5').textContent = fifth;
