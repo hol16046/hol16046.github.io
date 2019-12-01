b5ed56f901b33fd4381f823993bedc9f7b2fa9bb
hol16046 <hol16046@byui.edu>
2019-12-01 00:07:54 -0700

    Optimization

--- lesson11/js/preston.js
+++ lesson11/js/preston.js
@@ -16,7 +16,7 @@ const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
+        // console.log(jsObject);
 
         document.getElementById('temp').textContent = jsObject.main.temp.toFixed(0);
         document.getElementById('condition').textContent = jsObject.weather[0].description;
@@ -29,7 +29,7 @@ const apiURLforecast = 'https://api.openweathermap.org/data/2.5/forecast?id=5604
 fetch(apiURLforecast)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
+        // console.log(jsObject);
 
         var num = 1;
 
