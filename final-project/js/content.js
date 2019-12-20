9a4ae6ec750e34eccafbfb6c71e58185b58ff493
hol16046 <hol16046@byui.edu>
2019-12-20 15:56:10 -0700

    Final Project

---
+++ final-project/js/content.js
@@ -0,0 +1,7 @@
+function changeGuests(level) {
+    document.getElementById("currentGuests").innerHTML = level;
+}
+
+function changeRooms(level) {
+    document.getElementById("currentRooms").innerHTML = level;
+}
